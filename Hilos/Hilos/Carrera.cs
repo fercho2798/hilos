a243e10890e3dd40c9f131039df9b84821fb0c01
fercho2798 <47802477+fercho2798@users.noreply.github.com>
2021-01-26 14:07:19 -0500

    Add files via upload

---
+++ Hilos/Hilos/Carrera.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Hilos
+{
+    public class Carrera
+    {
+        public static void Corredor(object obj)
+        {
+            var nombre = (string)obj;
+            var pasos = 0;
+            var random = new Random();
+            while (pasos < 1000)
+            {
+                pasos += random.Next(0,4);
+                Console.WriteLine("corredor {0} dio {1} pasos ",nombre, pasos);
+                Thread.Sleep(10);
+
+            }
+            Console.WriteLine("el corredor {0} llego a la meta",nombre);
+        }
+    }
+}
---
+++ Hilos/Hilos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Hilos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Thread corredor1 = new Thread(Carrera.Corredor);
+            Thread corredor2 = new Thread(Carrera.Corredor);
+            Thread corredor3 = new Thread(Carrera.Corredor);
+            Thread corredor4 = new Thread(Carrera.Corredor);
+
+            corredor1.Start("Anthony");
+            corredor2.Start("Tania");
+            corredor3.Start("Brayan");
+            corredor4.Start("Majo");
+
+            corredor1.Join();
+            corredor2.Join();
+            corredor3.Join();
+            corredor4.Join();
+
+
+        }
+    }
+}
